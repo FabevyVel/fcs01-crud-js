39daeaaa4db12d1f3fc4521de67eb300ed64ccb7
FabevyVel <vel@fabevy.com>
2023-01-30 22:01:21 +0530

    Add base files.'

---
+++ main.js
@@ -0,0 +1,76 @@
+function handleValidation(e) {
+
+  e.preventDefault();
+    
+  let formElement = this,
+	result, validElements = 0,
+	formData,
+    requiredElements = formElement.getElementsByClassName("required");
+  removeErrorMessages(formElement);
+  for (let i = 0; i < requiredElements.length; i++) {
+    result = checkRequired(requiredElements[i]);
+	if(result == "valid"){
+		validElements++;
+	}
+  }
+  
+  //Check if all elements are valid
+  if(validElements == requiredElements.length){
+	formData = collectFormData(formElement);
+	createRow(formData);
+  }
+}
+
+function collectFormData(formElement){
+	var newFormElement = new FormData(formElement);
+	let formDataObj = {};
+	newFormElement.forEach(function(value, prop){
+		formDataObj[prop] = value;
+	});
+	return formDataObj;
+}
+
+function createRow(rowData){
+	//Create row element
+	
+	for(key in rowData){
+		if(key !== "password" && key !== "cpassword"){
+			console.log(key, rowData[key]);
+			//Create td
+			//Add rowData[key] inside the td
+			//Insert the td inside the row
+		}
+	}
+	
+	//Insert the row inside the table "tableElement"
+}
+
+function checkRequired(element) {
+  let elementValue = element.value,
+    elementWrapper = element.parentNode;
+  if (elementValue == "") {
+    let alertElement = document.createElement("div");
+    alertElement.innerText = "This Field is Required";
+    alertElement.classList.add("error");
+    elementWrapper.append(alertElement);
+	return "not valid";
+  }else{
+	return "valid";
+  }
+}
+
+function removeErrorMessages(form) {
+  let errorElements = form.getElementsByClassName("error");
+  
+  for (let i = errorElements.length-1; i >= 0; i--) {
+    removeMe(errorElements[i]);
+  }
+}
+function removeMe(ele) {
+  ele.remove();
+}
+
+var signupElement = document.getElementById("signup");
+var tableElement = document.getElementById("table");
+
+signupElement.addEventListener("submit", handleValidation);
